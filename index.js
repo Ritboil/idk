abc02b670166554ac43075d20caa11e34fc91830
Ritboil <roi2020roi2020roi2020@gmail.com>
2025-04-11 14:18:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["כנסו לשרת!", "השרת עם התכנות הכי טוב בישראל!"];
+const statusMessages = ["!כנסו לשרת", "!השרת עם התכנות הכי טוב בישראל"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
